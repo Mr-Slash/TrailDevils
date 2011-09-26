ccdf7e1b7783cb40272c7b8f45717d7ee79a08c6
dgiampa <d.giampa@bluemail.ch>
2011-09-26 16:05:28 +0200

    included search components for searching tracks

--- src/ch/hsr/traildevil/TrailDevilsActivity.java
+++ src/ch/hsr/traildevil/TrailDevilsActivity.java
@@ -1,13 +1,33 @@
 package ch.hsr.traildevil;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class TrailDevilsActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		
+		handleIntent(getIntent());
+	}
+
+	@Override
+	protected void onNewIntent(Intent intent) {
+		setIntent(intent);
+		handleIntent(intent);
+	}
+
+	private void handleIntent(Intent intent) {
+		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+			String query = intent.getStringExtra(SearchManager.QUERY);
+			// doSearch(query);
+			Toast.makeText(getApplicationContext(), "TEST " + query, Toast.LENGTH_SHORT).show();
+		}
+	}
 }

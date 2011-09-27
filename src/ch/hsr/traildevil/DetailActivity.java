055e51bed220f7959c9500a326a1bf583a4d1fa0
Slash <s.felicioni@gmx.ch>
2011-09-28 00:35:13 +0200

    Added detail view

---
+++ src/ch/hsr/traildevil/DetailActivity.java
@@ -0,0 +1,15 @@
+package ch.hsr.traildevil;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.detail);
+		
+		
+	}
+}

8bfce0b97acb61b30209be6f705670b31b6a38f7
dgiampa <d.giampa@bluemail.ch>
2011-10-10 23:14:11 +0200

    added color centralisation

--- gen/ch/hsr/traildevil/R.java
+++ gen/ch/hsr/traildevil/R.java
@@ -10,22 +10,30 @@ package ch.hsr.traildevil;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f050003;
+        public static final int dark_blue=0x7f050000;
+        public static final int dark_green=0x7f050001;
+        public static final int dark_grey=0x7f050005;
+        public static final int dark_red=0x7f050004;
+        public static final int light_green=0x7f050002;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
         public static final int map_marker=0x7f020001;
         public static final int trail_flims=0x7f020002;
     }
     public static final class id {
-        public static final int afternoon=0x7f070005;
-        public static final int detailview_mapview=0x7f070001;
-        public static final int favorites=0x7f070007;
-        public static final int friendlist=0x7f070008;
-        public static final int icon=0x7f070002;
-        public static final int morning=0x7f070004;
-        public static final int settings=0x7f070009;
-        public static final int status=0x7f070006;
-        public static final int statusLabelContainer=0x7f070000;
-        public static final int trackname=0x7f070003;
+        public static final int afternoon=0x7f080005;
+        public static final int detailview_mapview=0x7f080001;
+        public static final int favorites=0x7f080007;
+        public static final int friendlist=0x7f080008;
+        public static final int icon=0x7f080002;
+        public static final int morning=0x7f080004;
+        public static final int settings=0x7f080009;
+        public static final int status=0x7f080006;
+        public static final int statusLabelContainer=0x7f080000;
+        public static final int trackname=0x7f080003;
     }
     public static final class layout {
         public static final int detail=0x7f030000;
@@ -33,35 +41,33 @@ public final class R {
         public static final int tracklist_item=0x7f030002;
     }
     public static final class menu {
-        public static final int tracklist_menu=0x7f060000;
+        public static final int tracklist_menu=0x7f070000;
     }
     public static final class string {
         /**  Browser View 
+ Browser View 
          */
-        public static final int app_name=0x7f050000;
-        public static final int detailview_openinghours=0x7f05000a;
-        public static final int detailview_pricing=0x7f05000b;
+        public static final int app_name=0x7f060000;
+        public static final int detailview_openinghours=0x7f06000a;
+        public static final int detailview_pricing=0x7f06000b;
         /**  Detail View 
  Detail View 
          */
-        public static final int detailview_status=0x7f050003;
-        public static final int detailview_status_close=0x7f050005;
-        public static final int detailview_status_open=0x7f050004;
-        public static final int detailview_status_weather_afternoon=0x7f050007;
-        public static final int detailview_status_weather_morning=0x7f050006;
-        public static final int detailview_status_weather_rainy=0x7f050008;
-        public static final int detailview_status_weather_suny=0x7f050009;
-        /**  Browser View 
-         */
-        public static final int hello=0x7f05000d;
-        public static final int map_api_key=0x7f05000c;
-        public static final int no_tracks=0x7f050002;
-        public static final int search_hint=0x7f050001;
+        public static final int detailview_status=0x7f060003;
+        public static final int detailview_status_close=0x7f060005;
+        public static final int detailview_status_open=0x7f060004;
+        public static final int detailview_status_weather_afternoon=0x7f060007;
+        public static final int detailview_status_weather_morning=0x7f060006;
+        public static final int detailview_status_weather_rainy=0x7f060008;
+        public static final int detailview_status_weather_suny=0x7f060009;
+        public static final int map_api_key=0x7f06000c;
+        public static final int no_tracks=0x7f060002;
+        public static final int search_hint=0x7f060001;
         /**  Tracklist Option-Menu 
          */
-        public static final int tracklist_menu_favorites=0x7f05000e;
-        public static final int tracklist_menu_friendlist=0x7f05000f;
-        public static final int tracklist_menu_settings=0x7f050010;
+        public static final int tracklist_menu_favorites=0x7f06000d;
+        public static final int tracklist_menu_friendlist=0x7f06000e;
+        public static final int tracklist_menu_settings=0x7f06000f;
     }
     public static final class xml {
         public static final int searchable=0x7f040000;

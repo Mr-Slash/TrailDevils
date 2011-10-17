d997db768aa4f14e9e9ce6dc0caaff205fbe2411
Slash <s.felicioni@gmx.ch>
2011-10-17 15:07:04 +0200

    Added a progress bar at the beginning

--- src/ch/hsr/traildevil/TrailDevilsActivity.java
+++ src/ch/hsr/traildevil/TrailDevilsActivity.java
@@ -3,11 +3,16 @@ package ch.hsr.traildevil;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.app.Dialog;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
@@ -18,7 +23,12 @@ import ch.hsr.traildevil.domain.Trail;
 
 public class TrailDevilsActivity extends ListActivity {
 
-	TrailDevilsController controller;
+	private static final String TAG = "traildevil";
+	private static final String TAG_PREFIX = TrailDevilsActivity.class.getSimpleName() + ": ";
+	private static final int DIALOG_PROGRESS_ID = 0;
+	
+	private TrailDevilsController controller;
+	private Thread syncThread;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -37,6 +47,8 @@ public class TrailDevilsActivity extends ListActivity {
 
 		setListAdapter(new TrailsAdapter(this, R.layout.tracklist_item, trails));
 		handle(getIntent());
+		
+		showDialog(DIALOG_PROGRESS_ID);
 	}
 	
 	@Override
@@ -44,12 +56,6 @@ public class TrailDevilsActivity extends ListActivity {
 		super.onPause();
 		controller.close();
 		controller = null;
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		controller = new TrailDevilsController(getDir("data", Context.MODE_PRIVATE).toString());
 	}
 
 	@Override
@@ -79,7 +85,35 @@ public class TrailDevilsActivity extends ListActivity {
 		Trail trail = (Trail) listView.getAdapter().getItem(position);
 		Intent detail = new Intent(this, DetailActivity.class);
 		detail.putExtra("trailName", trail.getName());
-		detail.putExtra("trailId", trail.getTrailId());
+		detail.putExtra("trailPosition", position);
 		startActivity(detail);
 	}
+	
+	/**
+	 * Show a dialog with a progress bar. The user is informed, that the local data
+	 * is updated.
+	 */
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		final ProgressDialog dialog = new ProgressDialog(this);
+		dialog.setMessage("Check if new data is available..");
+		dialog.setIndeterminate(true);
+		dialog.setCancelable(true);
+		dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+			public void onCancel(DialogInterface dialog) {
+				Log.i(TAG, TAG_PREFIX + "Dialog cancel has been invoked");
+				if (syncThread != null) {
+					syncThread.interrupt();
+					finish();
+				}
+			}
+		});
+		dialog.setButton(Dialog.BUTTON_NEGATIVE, "Cancel", new OnClickListener() {
+			
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
+		});
+		return dialog;
+	}	
 }

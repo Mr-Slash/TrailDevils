dc6f4b4ffdae1137325bc4dd1d320f85fbc69c5f
dgiampa <d.giampa@bluemail.ch>
2011-11-19 17:29:17 +0100

    added package architecture

--- gen/ch/hsr/traildevil/R.java
+++ gen/ch/hsr/traildevil/R.java
@@ -53,12 +53,12 @@ public final class R {
         public static final int detailview_status=0x7f070003;
         public static final int detailview_weather=0x7f070004;
         public static final int icon=0x7f070007;
-        public static final int status=0x7f07000b;
+        public static final int status=0x7f07000a;
         public static final int statusLabel=0x7f070002;
         public static final int statusLabelContainer=0x7f070000;
         public static final int trackname=0x7f070008;
-        public static final int traillist_country=0x7f070009;
-        public static final int traillist_ratingbar=0x7f07000a;
+        public static final int traillist_country=0x7f07000b;
+        public static final int traillist_ratingbar=0x7f070009;
     }
     public static final class layout {
         public static final int detail=0x7f030000;
